436995e4a958eef8bd14b1b07435d8f7a1156e02
Nahum Alarcon <nahum.alarcon@cision.com>
2021-11-29 02:54:31 -0600

    Added schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE track (
+	id SERIAL PRIMARY KEY,
+	isrc VARCHAR ( 200 ) UNIQUE NOT NULL,
+	name VARCHAR ( 500 ) NOT NULL,
+	duration NUMERIC ( 12 ) NOT NULL,
+	explicit BOOLEAN NOT NULL
+);
+
