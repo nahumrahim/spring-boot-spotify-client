aecbd5d26f245f65efc44ad6f2a870c73ecc8f68
Nahum Alarcon <nahum.alarcon@cision.com>
2021-12-07 16:16:58 -0600

    Changed for constructor injection

--- src/main/kotlin/com/nahum/spotifyclient/rest/CodingChallengeController.kt
+++ src/main/kotlin/com/nahum/spotifyclient/rest/CodingChallengeController.kt
@@ -9,13 +9,10 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
-class CodingChallengeController {
-
-    @Autowired
-    val trackRepository: TrackRepository? = null;
-
-    @Autowired
-    val spotifyClientService: SpotifyClientService? = null;
+class CodingChallengeController @Autowired constructor(
+    private val trackRepository: TrackRepository,
+    private val spotifyClientService: SpotifyClientService
+){
 
     /**
      * Get all track list.
@@ -24,7 +21,7 @@ class CodingChallengeController {
      */
     @GetMapping("/tracks")
     fun getAllTracks(): MutableIterable<Track>? {
-        return trackRepository?.findAll()
+        return trackRepository.findAll()
     }
 
     /**
@@ -36,7 +33,7 @@ class CodingChallengeController {
      */
     @GetMapping("/codingchallenge/getTrack")
     fun getTrack(@RequestParam("isrc") isrc: String): Track? {
-        val localTrack = trackRepository?.findByIsrc(isrc)
+        val localTrack = trackRepository.findByIsrc(isrc)
             ?: throw ResponseStatusException(
                 HttpStatus.SC_NOT_FOUND, "Track not found.", null
             )
@@ -53,19 +50,19 @@ class CodingChallengeController {
      */
     @PostMapping("/codingchallenge/createTrack")
     fun createTrack(@RequestParam("isrc") isrc: String): Track? {
-        val localTrack = trackRepository?.findByIsrc(isrc)
+        val localTrack = trackRepository.findByIsrc(isrc)
         if (localTrack != null) {
             throw ResponseStatusException(
                 HttpStatus.SC_CONFLICT, "Record exists!", null
             )
         }
 
-        val remoteSpotifyTrack = spotifyClientService?.searchTracksSync(isrc)
+        val remoteSpotifyTrack = spotifyClientService.searchTracksSync(isrc)
             ?: throw ResponseStatusException(
                 HttpStatus.SC_NOT_FOUND, "Remote record not found.", null
             );
 
         var result = Track(isrc, remoteSpotifyTrack.name, remoteSpotifyTrack.durationMs, remoteSpotifyTrack.isExplicit);
-        return trackRepository?.save<Track>(result)
+        return trackRepository.save<Track>(result)
     }
 }
